11e5a28be5586e87621a5b864e6a4ebd763371d8
benjdj6 <benjdj6@vt.edu>
2016-11-23 12:20:13 -0500

    Created get flights stub endpoint
    currently just returns origin back to client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var requests = require('requests');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Flygo' });
+});
+
+router.get('/flights/:origin', function(req, res, next) {
+  res.json(req.params);
 });
 
 module.exports = router;
