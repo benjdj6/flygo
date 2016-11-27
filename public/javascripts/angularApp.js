2de91efcec688ca7c116a80261033af9232286ea
benjdj6 <benjdj6@vt.edu>
2016-11-27 12:42:50 -0500

    Form now sends data to angularApp

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flygo', []);
 
 app.factory('flights', [function() {
   var o = {
@@ -6,7 +6,11 @@ app.factory('flights', [function() {
   };
 
   //Sends request to Express API to get flights
-  o.getFlights = function(search) {
+  o.getFlights = function(query) {
+    if(!query.origin) {
+      alert("No origin defined");
+      return;
+    }
     console.log("request to get flights");
   };
 
@@ -21,8 +25,16 @@ app.controller('MainCtrl', [
     $scope.flights = flights.tickets;
     $scope.continents = [];
     $scope.getTickets = function() {
-      flights.getFlights({holder: null});
+      flights.getFlights({
+        origin: $scope.origin,
+        budget: $scope.budget,
+        alliance: $scope.alliance,
+        depart: $scope.depart,
+        return: $scope.return,
+        layover: $scope.layover,
+        domestic: $scope.domestic
+      });
     };
-  };
+  }
 ]);
 
