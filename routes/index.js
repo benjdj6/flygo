b8c530532da023ea691775a486159e9699ff2aca
benjdj6 <benjdj6@vt.edu>
2016-11-29 21:05:44 -0500

    Updated flights endpoint to return with human readable locations
    Loaded airports.json in index.js and go through flights and convert
    airport codes into readable city and airport names

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var airports = require('../data/airports.json');
 
 var router = express.Router();
 
@@ -23,7 +24,16 @@ router.get('/flights/:origin', function(req, res, next) {
   };
   request(options, function(err, response, body) {
     if(!err && response.statusCode == 200) {
-      res.json((JSON.parse(response.body)).FareInfo);
+      var flights = (JSON.parse(response.body)).FareInfo;
+      for(i = 0; i < flights.length; ++i) {
+        for(j = 0; j < airports.length; ++j) {
+          if(airports[j].code == flights[i].DestinationLocation) {
+            flights[i].DestinationLocation = airports[j].location;
+            break;
+          }
+        }
+      }
+      res.json(flights);
     }
     else {
       res.json(response.statusCode);
