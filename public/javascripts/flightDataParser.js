550260843b5dbb5f45d10fc8bd0e1c917fbf71b3
benjdj6 <benjdj6@vt.edu>
2016-12-10 13:51:12 -0500

    Flight parser now gets airline names and alliances

--- public/javascripts/flightDataParser.js
+++ public/javascripts/flightDataParser.js
@@ -1,10 +1,49 @@
-exports.parseFlightData = function(destination, flights) {
-  for(var key in flights) {
-    flights[key].DestinationLocation = destination.Destination.DestinationLocation;
-    flights[key].CityName = destination.Destination.CityName;
-    flights[key].CountryName = destination.Destination.CountryName;
-    flights[key].Fare = flights[key].AirItineraryPricingInfo.ItinTotalFare.TotalFare.Amount;
+var airlines = require('../../data/airports.json');
+
+var airlineCodes = {};
+
+function getAirline(itinerary, flight) {
+  flight.Airline = itinerary.OriginDestinationOption[0].FlightSegment[0].MarketingAirline.Code;
+  if(airlineCodes[flight.Airline]) {
+    flight.Airline = airlineCodes[flight.Airline].name
+    flight.Alliance = airlineCodes[flight.Airline].alliance
+  }
+  else {
+    for(i = 0; i < airlines.length; ++i) {
+      if(airlines[i].code == flight.Airline) {
+        flight.Airline = airlines[i].name;
+        flight.Alliance = airlines[i].alliance;
+        
+        airlineCodes[airlines[i].code] = {
+          'name' : airlines[i].name,
+          'alliance' : airlines[i].alliance
+        };
+        break;
+      }
+    }
   }
 
-  return flights;
+  return flight;
+}
+
+function getLayover(itinerary, flight) {
+  //DO THINGS
+  return flight;
+}
+
+exports.parseFlightData = function(destination, flights) {
+  parsedData = []
+  for(var key in flights) {
+    flight = {
+      'DestinationLocation' : destination.Destination.DestinationLocation,
+      'CityName' : destination.Destination.CityName,
+      'CountryName' : destination.Destination.CountryName,
+      'Fare' : flights[key].AirItineraryPricingInfo.ItinTotalFare.TotalFare.Amount,
+    }
+    flight = getAirline(flights[key].AirItinerary.OriginDestinationOptions, flight);
+    parsedData.push(flight);
+    console.log(flights[key].AirItinerary.OriginDestinationOptions.OriginDestinationOption[0].FlightSegment[0].MarketingAirline.Code);
+  }
+
+  return parsedData;
 }
